b99f3656949402be0296868e6a0f14bcaf2768d1
haixzoyuchan <wildarahman14.com>
2022-03-14 15:47:07 +0700

    commit pertama

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="/css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaravelWilda</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Selamat Datang</h2>
+        <form action="">
+        <input type="email" placeholder="Destinasi Tujuan" required>
+        <input type="text" placeholder="Transportasi" required>
+        <input type="date" placeholder="Tanggal keberangkatan" required>
+        <input type="date" placeholder="Tanggal kembali" required>
+        <input type="text" placeholder="Menu VIP" required>
+           <div class="btns">
+            <button type="submit">Konfirmasi Pemesanan</button>
+            <a href="./.html"><button</a>
+           </div>
+            
+        </form>
+    </div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="/css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaravelWilda</title>
+   
+</head>
+<body>
+    
+    <div class="container">
+        <h2>Selamat Datang</h2>
+        <form action="">
+        <input type="email" placeholder="Email" required>
+        <input type="text" placeholder="Nama" required>
+        <input type="text" placeholder="Tujuan" required>
+        <input type="date" placeholder="Tanggal keberangkatan" required>
+        <input type="date" placeholder="Tanggal kembali" required>s
+           <div class="btns">
+            <button type="submit">Konfirmasi Pemesanan</button>
+            <a href="./registration.html"><button</a>
+           </div>
+            
+        </form>
+    </div>
+</body>
+</html>
